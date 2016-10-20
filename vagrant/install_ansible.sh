0ae608a21256e2c80bfe78c553ce83d69cead6a5
kmisachenka <misachenka@gmail.com>
2016-10-20 16:33:05 +0300

    Add tasks for redhat like sytems

--- vagrant/install_ansible.sh
+++ vagrant/install_ansible.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Installing Ansible..."
+echo "$(tput setaf 1)Installing Ansible...$(tput sgr0)"
+
 apt-get install -y software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install -y --force-yes ansible
+
+echo "$(tput setaf 1)ansible --version$(tput sgr0)"
+ansible --version
